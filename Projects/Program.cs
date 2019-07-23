094fbd6afd2d1a78c81c6ee63e2858ed7794af15
Ryan Pethel <arpethel@gmail.com>
2019-07-23 10:40:14 -0400

    Initial commit. Project setup.
    This establishes the foundation on which we build our game.

---
+++ Projects/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Projects
+{
+    class MainClass
+    {
+        static char[,] screen = new char[9, 9];
+        static string display = "";
+        static int critterX = 4;
+        static int shipX = 4;
+
+
+        static void ViewDisplay()
+        {
+
+            Console.Clear();
+        }
+
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
